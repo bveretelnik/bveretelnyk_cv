0ae11e4115bff91cf456b9415e4017ba300ae1e9
Serhii Serohin <s.serohin@betby.com>
2025-06-10 23:52:29 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+            <h1 className="text-4xl font-bold">My Resume (Next.js + Tailwind)</h1>
+        </div>
+    );
+}
